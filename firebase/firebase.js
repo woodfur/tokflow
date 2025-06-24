fbd16bc01a7c8e7ec92c4740d50da1f1bd959157
Timothy Olufemi Williams <dev.woodfur@outlook.com>
2025-06-24 10:32:55 +0000

    Unknow uodates

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
